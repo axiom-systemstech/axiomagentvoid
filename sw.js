a36ac7e99f9b19e1a99f21fb4d4693a068f8be68
Manuel Echepares | AXIOM Systems ◤A-S◢ <echeparesmanuel36@gmail.com>
2026-06-09 22:44:32 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = "axiom-agent-void-v1";
+const files = ["./", "./index.html", "./manifest.json", "./assets/favicon.png"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(files))));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
